f6fd5e06b74490bdaa77aecb3ffe41daaab8de55
Sadarius Bowser <sadariusbowser@Sadariuss-MacBook-Air.local>
2023-07-12 11:21:46 -0400

    added controller/routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -12,26 +12,6 @@ function show(req, res) {
   });
 }
 
-// function newTask(req, res) {
-//   res.render("tasks/new", { title: "Add Task", errorMsg: "" });
-// }
-
-// async function create(req, res) {
-//   try {
-//     const task = await Task.create(req.body);
-//     res.redirect(`/tasks/${task._id}`);
-//   } catch (err) {
-//     console.log(err);
-//     res.render("tasks/new", { errorMsg: err.message });
-//   }
-// }
-
-
-// async function show(req, res) {
-//   const task = await Task.findById(req.params.id).populate("cast");
-//   const notes = await Note.find({ _id: { $nin: task.cast } }).sort("name");
-//   res.render("tasks/show", { title: "Task Detail", movie, notes });
-// }
 
 module.exports = {
   index,
