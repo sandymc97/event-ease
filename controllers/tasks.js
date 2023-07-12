fb3c7cb2d99d8791bef66b077a89e110f7ff382f
Sadarius Bowser <sadariusbowser@Sadariuss-MacBook-Air.local>
2023-07-12 10:40:05 -0400

    added routes/controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,17 +1,41 @@
 const Task = require("../models/task");
+// const Note = require("../models/note");
 
-function index(req, res) {
-  res.render("tasks/index", {
-    tasks: Task.getAll(),
-  });
+async function index(req, res) {
+  const tasks = await Task.find({});
+  res.render("tasks/index", { title: "All Tasks", tasks });
 }
+
 function show(req, res) {
   res.render("tasks/show", {
     task: Task.getOne(req.params.id),
   });
 }
 
+// function newTask(req, res) {
+//   res.render("tasks/new", { title: "Add Task", errorMsg: "" });
+// }
+
+// async function create(req, res) {
+//   try {
+//     const task = await Task.create(req.body);
+//     res.redirect(`/tasks/${task._id}`);
+//   } catch (err) {
+//     console.log(err);
+//     res.render("tasks/new", { errorMsg: err.message });
+//   }
+// }
+
+
+// async function show(req, res) {
+//   const task = await Task.findById(req.params.id).populate("cast");
+//   const notes = await Note.find({ _id: { $nin: task.cast } }).sort("name");
+//   res.render("tasks/show", { title: "Task Detail", movie, notes });
+// }
+
 module.exports = {
   index,
-  show
+  show,
+  // new: newTask,
+  // create
 };
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,11 +2,7 @@ var express = require('express');
 var router = express.Router();
 var tasksCtrl = require("../controllers/tasks");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-// GET /tasks
+/// ALL ROUTES DEFAULT TO /tasks
 router.get("/", tasksCtrl.index);
 
 // GET /tasks/:id
