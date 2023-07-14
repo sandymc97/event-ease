1f1497d44a01391cfd54c690dc93047e8d9c5817
Darius Bowser <134982506+DariusBowser@users.noreply.github.com>
2023-07-14 11:33:09 -0400

    updated function in mentalStatus, and show.ejs

--- routes/mentalstatuses.js
+++ routes/mentalstatuses.js
@@ -6,6 +6,6 @@ const mentalStatusesCtrl = require('../controllers/mentalstatuses');
 // POST /movies/:id/reviews 
 router.post('/tasks/:id/mentalstatuses', mentalStatusesCtrl.create);
 // DELETE /mental status
-router.delete('/mentalstatuses/:id', mentalStatusesCtrl.delete);
+router.delete('/mentalstatuses/:id/:taskid', mentalStatusesCtrl.delete);
 
 module.exports = router;
