2d7f19eec951eba539093a67c6226669af402c02
Sandy <sandyahmedy7@gmail.com>
2023-07-16 21:45:55 -0500

    main now

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,37 +1,45 @@
-const Task = require("../models/task");
-
-// const Note = require("../models/note");
-// Before Merge
-
-async function index(req, res) {
-  const tasks = await Task.find({});
+const taskDB = require("../models/task");
+async function findAllTask(_req, res) {
+  const tasks = await taskDB.find({});
   res.render("tasks/index", { title: "All Tasks", tasks });
 }
-
-async function show(req, res) {
-  const task = await Task.findById(req.params.id)
-  res.render("tasks/show", { title: "task Detail", task});
+async function showTaskDetails(req, res) {
+  const task = await taskDB.findById(req.params.id);
+  res.render("tasks/show", { title: "Task Detail", task });
 }
-
-function newTask(req, res) {
-  // We'll want to be able to render an
-  // errorMsg if the create action fails
-  res.render("tasks/index", { title: "Add Task", errorMsg: "" });
-}
-
-async function create(req, res) {
-console.log(req.body)
+async function createNewTask(req, res) {
   try {
-    const task = await Task.create(req.body);
+    await taskDB.create(req.body);
     res.redirect("/tasks");
   } catch (err) {
     res.redirect("/tasks", { errorMsg: err.message });
   }
 }
-
+async function deleteTask(req, res) {
+  try {
+    await taskDB.deleteOne({ _id: req.params.id });
+    res.redirect("/tasks");
+  } catch (err) {
+    res.redirect("/tasks", { errorMsg: err.message });
+  }
+}
+async function editTask(req, res) {
+  try {
+    await taskDB.updateOne({ _id: req.params.id }, req.body);
+    res.redirect("/tasks");
+  } catch (error) {
+    res.redirect("/tasks", { errorMsg: error.message });
+  }
+}
 module.exports = {
-  index,
-  show,
-  new: newTask,
-  create
+  findAllTask,
+  showTaskDetails,
+  createNewTask,
+  deleteTask,
+  editTask,
 };
+
+
+
+
+
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,15 +2,19 @@ var express = require('express');
 var router = express.Router();
 var tasksCtrl = require("../controllers/tasks");
 // const ensureLoggedIn = require('../config/ensureLoggedIn');
-
 /// ALL ROUTES DEFAULT TO /tasks
-router.get("/", tasksCtrl.index);
-
-
+router.get("/", tasksCtrl.findAllTask);
 // GET /tasks/:id
-router.get("/:id", tasksCtrl.show);
+router.get("/:id", tasksCtrl.showTaskDetails);
 // POST /tasks
-router.post("/", tasksCtrl.create);
-
-
+router.post("/", tasksCtrl.createNewTask);
+// DELETE /tasks/:id
+router.delete("/:id", tasksCtrl.deleteTask);
+// PUT tasks
+router.post("/:id", tasksCtrl.editTask);
 module.exports = router;
+
+
+
+
+
